fd54777bc87fb5e96c0dce23528dc468dca2c8d1
Joshua <Doerak1aug@gmail.com>
2022-05-23 14:46:04 +0200

    created session playlist hud, styling changes

---
+++ resources/views/components/tempPlaylist.blade.php
@@ -0,0 +1,24 @@
+<div style="position:fixed; bottom: 0; right: 0; width: 14%; border:gray 1px solid; border-radius: 10px 0px 0px 0px;">
+    <div style="background-color: gray; border-radius: 10px 0px 0px 0px; height: 20px; padding: 10px;">
+        <div style="float:left;">{{count($songs)}}</div>
+        <div style="float:right;">XX:xx</div>
+    </div>
+    <div style="max-height: 600px; overflow-y: scroll;">
+        <div style="display:flex; padding: 10px 5px 10px 10px;">
+            <div style="display: inline-block; text-overflow: ellipsis; overflow-x: hidden; white-space: nowrap; width: 70%; height: 20px" title="">You</div>
+            <div style="display: inline-block; width: 30%; text-align: center;">du:ra</div>
+        </div>
+        <div style="display:flex; padding: 10px 5px 10px 10px;">
+            <div style="display: inline-block; text-overflow: ellipsis; overflow-x: hidden; white-space: nowrap; width: 70%; height: 20px" title="">creature</div>
+            <div style="display: inline-block; width: 30%; text-align: center;">du:ra</div>
+        </div>
+        <div style="display:flex; padding: 10px 5px 10px 10px;">
+            <div style="display: inline-block; text-overflow: ellipsis; overflow-x: hidden; white-space: nowrap; width: 70%; height: 20px" title="">secret for the mad</div>
+            <div style="display: inline-block; width: 30%; text-align: center;">du:ra</div>
+        </div>
+        <div style="display:flex; padding: 10px 5px 10px 10px;">
+            <div style="display: inline-block; text-overflow: ellipsis; overflow-x: hidden; white-space: nowrap; width: 70%; height: 20px" title="I like (the idea of) you">I like (the idea of) you</div>
+            <div style="display: inline-block; width: 30%; text-align: center;">du:ra</div>
+        </div>
+    </div>
+</div>
--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -7,7 +7,7 @@
     <body>
         @include('components/navigate')
         
-        <div style="width:75%; margin-left: 12.5%; padding-top: 20px;">
+        <div style="width:70%; margin-left: 15%; padding-top: 20px;">
             <div style="position: relative; height: 60px; padding: 20px 0px; margin-bottom: 20px; border-radius: 23px; background-color: {{$title[0]['theme']}}">
                 <span class="genreTitle">{{$title[0]['name']}}</span>
             </div>
@@ -17,6 +17,7 @@
                         <div style="width: 100%; text-align: center; display: inline-block">Nothing Here Yet!</div>
                 </div>
             @endif
+            <div style="height: 500px; overflow-y: scroll; ">
             @foreach($songs as $song)
             <div style="display:flex; align-items: flex-start;"> 
                 <div style="display: inline-block; float: left; width:5%; height:50px; border-radius: 23px 0px 0px 23px; background-color: {{$themes[$song->genre_id -1]->theme}};"></div>
@@ -37,11 +38,13 @@
 
                 </div>
 
-                <div style="display: inline-block; padding: 15px 0px; width: 3%; border-radius: 0px 23px 23px 0px; background-color: {{$themes[$song->genre_id -1]->theme}};">
+                <a href='/sessionAdd/{{$song->id}}' style="display: inline-block; padding: 15px 0px; width: 3%; border-radius: 0px 23px 23px 0px; background-color: {{$themes[$song->genre_id -1]->theme}};">
                     <div style="text-align: center; width: 100%; line-height: 20px; border-radius: 10px; color:white; background-color:{{$themes[$song->genre_id -1]->theme}};">+</div>
-                </div>
+                </a>
             </div>
             @endforeach
+            </div>
         </div>
+        @include('components/tempPlaylist')
     </body>
 </html>
